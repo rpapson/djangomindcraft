9f5c338a1dda11002b3c135ad2a6565949c88384
rpapson <rpapson@gmail.com>
2022-04-14 13:52:18 -0700

    Added accordian style dropdown as an experiment

--- tarot_cards/urls.py
+++ tarot_cards/urls.py
@@ -5,5 +5,6 @@ app_name = 'tarot_cards'
 
 urlpatterns = [
     path('',views.card_view),
-    path('list_cards/',views.list_cards_view)
+    path('list_cards/',views.list_cards_view),
+    path('accordian_cards/',views.card_accordian_view)
     ]
--- tarot_cards/views.py
+++ tarot_cards/views.py
@@ -9,6 +9,12 @@ def card_view(request):
 
     return render(request, 'tarot_cards/card.html', context = choice)
 
+def card_accordian_view(request):
+    result = TarotCard.objects.order_by('?').first()
+    choice = {'title':result.title,'src':result.src,'text':result.text}
+
+    return render(request, 'tarot_cards/card_accordian.html', context = choice)
+
 def list_cards_view(request):
     result = TarotCard.objects.all()
     context = {'cards':result}
